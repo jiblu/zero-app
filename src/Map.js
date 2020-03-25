79787bf7e14d6e4f0cb85ecdabbcf0690fadf8d2
ji <jkim0107@gmail.com>
2020-03-25 09:50:09 -0700

    added pin functionality

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react'
 import ReactMapGL from 'react-map-gl'
+import {SVGOverlay, HTMLOverlay, CanvasOverlay} from 'react-map-gl'
+import Pins from './Pins'
+
+import DRIVERS from './drivers.json'
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAP_GL_TOKEN
 
@@ -16,12 +20,19 @@ class Map extends Component {
   }
 
   render() {
+    function redraw({project}) {
+      const [cx, cy] = project([-122, 37])
+      return <circle cx={cx} cy={cy} r={40} fill="blue" />
+    }
     return (
       <ReactMapGL
         {...this.state.viewport}
         onViewportChange={(viewport) => this.setState({viewport})}
         mapboxApiAccessToken={MAPBOX_TOKEN}
-      />
+      >
+        <Pins data={DRIVERS} />
+        <SVGOverlay redraw={redraw} />
+      </ReactMapGL>
     )
   }
 }
